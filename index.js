5cb4a1eda914758f4f49307311d657b059ca757c
Dosh Barzanji <Doshbarzanji@gmail.com>
2026-02-14 13:10:45 -0700

    Resolve merge conflict and complete scope lab

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-
 // Step 1: Variable Data (Global Scope)
-var burgers = ['Hamburger', 'Cheeseburger'];
-var featuredDrink = 'Strawberry Milkshake';
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
 
 // Step 2: Function with Function-Scoped Variable
 function addBurger() {
-  var newBurger = 'Flatburger';  // Function-scoped
+  var newBurger = 'Flatburger';
   burgers.push(newBurger);
 }
 
 // Step 3: Block-Scoped Variable
 if (true) {
-  let anotherNewBurger = 'Maple Bacon Burger';  // Block-scoped
+  let anotherNewBurger = 'Maple Bacon Burger';
   burgers.push(anotherNewBurger);
 }
 
 // Function to Change Featured Drink
 function changeFeaturedDrink() {
-  featuredDrink = 'The JavaShake';  // Modifies global variable
+  featuredDrink = 'The JavaShake';
 }
-
-// Write your solution in this file!
