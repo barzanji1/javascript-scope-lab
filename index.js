3a703e0dd5c13c46c7caf826360fc0beb4a0cc9e
Dosh Barzanji <Doshbarzanji@gmail.com>
2026-02-14 12:51:59 -0700

    Add initial Flatburger Restaurant JS logic

---
+++ index.js
@@ -0,0 +1,20 @@
+// Step 1: Variable Data (Global Scope)
+var burgers = ['Hamburger', 'Cheeseburger'];
+var featuredDrink = 'Strawberry Milkshake';
+
+// Step 2: Function with Function-Scoped Variable
+function addBurger() {
+  var newBurger = 'Flatburger';  // Function-scoped
+  burgers.push(newBurger);
+}
+
+// Step 3: Block-Scoped Variable
+if (true) {
+  let anotherNewBurger = 'Maple Bacon Burger';  // Block-scoped
+  burgers.push(anotherNewBurger);
+}
+
+// Function to Change Featured Drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';  // Modifies global variable
+}
